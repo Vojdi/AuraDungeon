ef8de5dd72caddf59369978e9ad2acfd25530426
Vojdi <161755899+Vojdi@users.noreply.github.com>
2025-04-26 22:02:58 +0200

    fixes

--- Assets/Scripts/Canvas/MainMenu.cs
+++ Assets/Scripts/Canvas/MainMenu.cs
@@ -14,6 +14,11 @@ public class MainMenu : MonoBehaviour
     [SerializeField] Button[] buttons;
 
     bool Chosen = false;
+
+    // Your color settings
+    Color selectedColor = new Color32(0xF5, 0xF5, 0xF5, 0xFF);   // #F5F5F5
+    Color deselectedColor = new Color32(0x6B, 0x6A, 0x6A, 0xFF); // #6B6A6A
+
     public void StartGame()
     {
         if (Chosen)
@@ -21,10 +26,12 @@ public class MainMenu : MonoBehaviour
             SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
         }
     }
+
     public void ExitGame()
     {
         Application.Quit();
     }
+
     public void GoToCharacterSelection()
     {
         mainMenuGj.SetActive(false);
@@ -37,25 +44,34 @@ public class MainMenu : MonoBehaviour
         Chosen = false;
         beginButtonText.text = "Choose your Character first!";
     }
+
     public void ReturnToMainMenu()
     {
         mainMenuGj.SetActive(true);
         mainMenuOtherGraphicsGj.SetActive(true);
         characterSelectionScreenGj.SetActive(false);
     }
+
     public void ChooseButtonClicked(int buttonId)
     {
-        
-            
-        
-        foreach (var text in texts) {
-            text.text = "";
+        // Clear all selection text labels
+        for (int i = 0; i < texts.Length; i++)
+        {
+            texts[i].text = "";
         }
+
+        // Set selected label
         texts[buttonId].text = "Selected";
         PlayerPrefs.SetInt("charId", buttonId);
         Chosen = true;
         beginButtonText.text = "Begin";
-        buttons[buttonId].Select();
-    }
 
+        // Update button colors manually
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            var colors = buttons[i].colors;
+            colors.normalColor = (i == buttonId) ? selectedColor : deselectedColor;
+            buttons[i].colors = colors;
+        }
+    }
 }
